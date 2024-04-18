b3de9016db41f0ee38b97950c7991fe7d99a4489
Aniket6181 <emailid>
2024-04-18 07:27:43 +0000

    JavaTutorial Search

--- src/test/java/testScript1/GoogleTest.java
+++ src/test/java/testScript1/GoogleTest.java
@@ -1,14 +1,24 @@
 package testScript1;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GoogleTest {
 	
 	public static void main (String[] args) {
+		//ChromeOptions options =new ChromeOptions();
+		//options.setBrowserVersion("115");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com");
+		System.out.println("Page Title :" + driver.getTitle());
+		WebElement seachBox = driver.findElement(By.id("APjFqb"));
+		seachBox.sendKeys("Java Tutorial");
+		seachBox.sendKeys(Keys.ENTER);
 		System.out.println("Page Title :" + driver.getTitle());
 	}
 

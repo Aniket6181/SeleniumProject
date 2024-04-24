149af217b2b9ff43a9c7077a5467e26e9ef9ffe9
Aniket6181 <emailid>
2024-04-24 07:21:16 +0000

    iframes code

---
+++ src/test/java/testScript1/FramesTest.java
@@ -0,0 +1,39 @@
+package testScript1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FramesTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://chercher.tech/practice/frames-example-selenium-webdriver");
+		
+		driver.switchTo().frame("frame1");
+		WebElement inpBox = driver.findElement(By.xpath("//input[@type='text']"));
+		inpBox.sendKeys("Hello World");
+		
+		
+		//driver.switchTo().frame("frame3");
+		driver.switchTo().frame(0);
+		driver.findElement(By.id("a")).click();
+		
+		
+		driver.switchTo().parentFrame();
+		inpBox = driver.findElement(By.xpath("//input[@type='text']"));
+		inpBox.clear();
+		inpBox.sendKeys("Welcome Back");
+		
+		driver.switchTo().defaultContent();
+		
+		System.out.println(driver.findElement(By.xpath("//label//span")).getText());
+		
+		
+
+	}
+
+}
